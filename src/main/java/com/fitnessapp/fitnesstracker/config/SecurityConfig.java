298bfb1ffcaa74703ce30391421456c56d29b245
rvatsdeo <rvcipher1@gmail.com>
2025-08-30 19:33:10 +0530

    added cors specific webconfig files

--- src/main/java/com/fitnessapp/fitnesstracker/config/SecurityConfig.java
+++ src/main/java/com/fitnessapp/fitnesstracker/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf->csrf.disable())
+                .cors(cors->{})
                 .authorizeHttpRequests(auth->auth.
                         requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
